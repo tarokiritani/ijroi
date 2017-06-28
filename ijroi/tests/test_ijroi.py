eb5dddd0152e5214afcf47fd0ba6f48da236df82
Taro Kiritani <taro.kiritani@epfl.ch>
2017-06-28 17:17:26 +0200

    fix test

--- ijroi/tests/test_ijroi.py
+++ ijroi/tests/test_ijroi.py
@@ -29,46 +29,64 @@ def test_rectangle():
 def test_freehand_circle():
     fixture = get_fixture("freehand_circle.roi")
     with fixture.open("rb") as f:
-        circle = ijroi.read_roi(f)
+        circle_roi = ijroi.read_roi(f)
+        circle = circle_roi.points
+        position = circle_roi.position
     assert len(circle) == 100
     assert abs(circle[:, 1].mean()-10) < 0.01
     assert abs(circle[:, 0].mean()-15) < 0.01
+    assert position == 0
 
 def test_integer_freehand():
     fixture = get_fixture("freehand_integer.roi")
     with fixture.open("rb") as f:
-        freehand = ijroi.read_roi(f)
+        freehand_roi = ijroi.read_roi(f)
+        freehand = freehand_roi.points
+        position = freehand_roi.position
     assert len(freehand) == 3
     assert all(freehand[2, :] == [1, 10])
     assert freehand.dtype == np.int16
+    assert position == 0
 
 def test_polygon():
     fixture = get_fixture("polygon_circle.roi")
     with fixture.open("rb") as f:
-        circle = ijroi.read_roi(f)
+        circle_roi = ijroi.read_roi(f)
+        circle = circle_roi.points
+        position = circle_roi.position
     assert len(circle) == 100
     assert abs(circle[:, 1].mean()-10) < 0.01
     assert abs(circle[:, 0].mean()-15) < 0.01
+    assert position == 0
 
     fixture = get_fixture("polygon_integer.roi")
     with fixture.open("rb") as f:
-        polyint = ijroi.read_roi(f)
+        polyint_roi = ijroi.read_roi(f)
+        polyint = polyint_roi.points
+        position = polyint_roi.position
     assert len(polyint) == 3
     assert all(polyint[2, :] == [1, 10])
     assert polyint.dtype == np.int16
+    assert position == 0
 
 def test_point():
     fixture = get_fixture("int_point.roi")
     with fixture.open("rb") as f:
-        point = ijroi.read_roi(f)
+        point_roi = ijroi.read_roi(f)
+        point = point_roi.points
+        position = point_roi.position
     assert point.ndim == 2
     assert point[0,0] == 256
     assert point[0,1] == 128
+    assert position == 0
 
 def test_float_point():
     fixture = get_fixture("float_point.roi")
     with fixture.open("rb") as f:
-        point = ijroi.read_roi(f)
+        point_roi = ijroi.read_roi(f)
+        point = point_roi.points
+        position = point_roi.position
     assert point.ndim == 2
     assert abs(point[0,0] - 567.8) < 0.01
     assert abs(point[0,1] - 123.4) < 0.01
+    assert position == 0
